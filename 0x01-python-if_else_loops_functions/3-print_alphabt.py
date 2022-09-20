81d34a0ee6b2dd5c43cc1d409651d1990e1d704b
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 01:43:24 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the ASCII alphabet in lowercase except 'q' and 'e'
+# demonstrates how to use a loop, a loop clause and an if condition to
+# affect program output
+#
+# (C) 2022 Daniel Waweru, Nairobi, Kenya
+# email danielwaweru9632@gmail.com
+# -----------------------------------------------------------
+
+# For each ascii_code in range sequence
+for ascii_code in range(97, 123):
+
+    # If ascii_code match code for 'q' and 'e' skip it
+    if ascii_code == 101 or ascii_code == 113:
+        continue
+
+    # Print out the character format of the ascii_code
+    print("{:c}".format(ascii_code), end='')
