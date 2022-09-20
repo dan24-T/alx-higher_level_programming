1cb6f1c3ab103612c38c9f35fcb1e35d56e7e6ff
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 02:11:00 +0300

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to determine if a number stored in a variable is
-# positive or negative
-# demonstrates how to use if...elif conditions to determine the program output
-#
-# (C) 2022 Daniel Waweru, Nairobi, Kenya
-# email danielwaweru9632@gmail.com
-# -----------------------------------------------------------
-
-# Generate random numbers and assign it to a variable named number
 import random
 number = random.randint(-10000, 10000)
 
-# Get and store last digit of number in a variable named last_digit
-last_digit = int(repr(number)[-1])
-
-# Print out number and it's last digit followed by ...
-# ..." and is greater than 5", if the last digit is greater than 5
-if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-
-# ..." and is less than 6 and not 0", if the last digit is less than 6
-# and not 0
-elif last_digit < 6 and last_digit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0". format(number, last_digit))
-
-# ..." and is 0", if the last digit is 0
-elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+if number < 0:
+    dig = abs(number) % 10 * -1
+else:
+    dig = number % 10
+print("Last digit of {} is {} and is".format(number, dig), end=" ")
+if dig > 5:
+    print("greater than 5")
+elif dig == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
