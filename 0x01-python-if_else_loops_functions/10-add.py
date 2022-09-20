d7ed32a9863b7b16610ff48f0b21705f18e79d18
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 01:29:14 +0300

    Update 10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # -----------------------------------------------------------
 # Python program that:
-# demonstrates how to add two integers and return the result
-# demonstrates how to use a function to affect program output
+# demonstrates how to determine if a number stored in a variable is
+# positive or negative
+# demonstrates how to use if...elif conditions to determine the program output
 #
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
+# (C) 2022 Daniel Waweru, Nairobi, Kenya
+# email danielwaweru9632@gmail.com
 # -----------------------------------------------------------
 
 
