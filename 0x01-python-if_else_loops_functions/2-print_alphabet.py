045799b9cadaad88dec454fe2e354595591d924a
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 01:41:47 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the ASCII alphabet in lowercase
+# demonstrates how to use a loop to affect program output
+#
 # (C) 2022 Daniel Waweru, Nairobi, Kenya
 # email danielwaweru9632@gmail.com
+# -----------------------------------------------------------
+
+# For each ascii_code in range sequence
+for ascii_code in range(97, 123):
+
+    # Print out the character format of the ascii_code
+    print("{:c}".format(ascii_code), end='')
