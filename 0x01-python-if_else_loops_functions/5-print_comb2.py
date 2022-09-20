d2a686acf3b1497683b7c4fedeadb789774d29ac
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 01:47:30 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print numbers from 0 to 99
+# demonstrates how to use a loop and string formatting to affect program output
+#
+# (C) 2022 Daniel Waweru, Nairobi, Kenya
+# email danielwaweru9632@gmail.com
+# -----------------------------------------------------------
+
+# For each number in range sequence
+for number in range(0, 99):
+
+    # Print out number according to fill and width format
+    print("{:02d}".format(number), end=", ")
+
+# Print out last number
+print(99)
