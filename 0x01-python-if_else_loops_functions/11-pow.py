f8ab72754730edecc316846716d6eedcd974ba67
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 01:37:38 +0300

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to compute a to the power of b and return the value
+# demonstrates how to use a function to affect program output
+#
+# (C) 2022 Daniel Waweru, Nairobi, Kenya
+# email danielwaweru9632@gmail.com
+# -----------------------------------------------------------
+
+
+def pow(a, b):
+    """Raises a number to a power
+    Args:
+        a: number argument
+        b: power argument
+    Returns:
+        the result of exponentiation
+    """
+    return a**b
