26f94f76c8caae48ae990dc368cbbbc3203c2a9d
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 02:07:01 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print a string in uppercase
-# demonstrates how to use a function and if condition to affect program output
-#
-# (C) 2022 Daniel Waweru, Nairobi, Kenya
-# email danielwaweru9632@gmail.com
-# -----------------------------------------------------------
-
 def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) < ord('z') + 1:
