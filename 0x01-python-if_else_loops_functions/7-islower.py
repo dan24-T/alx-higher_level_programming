8b911b39951a5ee7740fc558cc95e484dd19f17f
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 01:50:40 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to check for lowercase character using ASCII code
+# demonstrates how to use a function and if condition to affect program output
+#
+# (C) 2022 Daniel Waweru, Nairobi, Kenya
+# email danielwaweru9632@gmail.com
+# -----------------------------------------------------------
+
+
+def islower(c):
+    """Checks for lowercase
+    Args:
+        c: a single character argument
+    Returns:
+        True: if c is lowercase
+        False: if c is not lowercase
+    """
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    return False
