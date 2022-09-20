5a32588bd3d283871f9a91897b1693a0a67a2533
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-09-21 01:33:13 +0300

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to create a copy of the string, removing the character at
+# the position n (not the Python way, the “C array index”)
+# demonstrates how to use a function, and an if...elif condition to affect
+# program output
+#
+# (C) 2022 Daniel Waweru, Nairobi, Kenya
+# email danielwaweru9632@gmail.com
+# -----------------------------------------------------------
+def remove_char_at(str, n):
+    """Removes the char at an index in a given string
+    Args:
+        str: string argument
+        n: index argument
+    Returns:
+        str: edited string
+    """
+
+    # If the index is a positive integer
+    if n >= 0:
+
+        # Use slicing to ommit the character at index n
+        str = str[:n] + str[n + 1:]
+
+    # Return editied string
+    return str
